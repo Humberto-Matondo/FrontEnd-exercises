00816194c4f15e789b95a00d13c20d22a5f1bb78
Humberto-Matondo <humbertomat99@gmail.com>
2024-07-07 05:38:27 +0100

    Interpolacao

--- JavaScript-lesson/lesson-2/js/script.js
+++ JavaScript-lesson/lesson-2/js/script.js
@@ -1,12 +1,16 @@
 // 1- Number
-console.log(typeof(2));
-console.log(typeof(2.3));
-console.log(typeof(-2));
+console.log(typeof 2);
+console.log(typeof 2.3);
+console.log(typeof -2);
 
 // 2- ESPECIAL NUMBERS
 
-console.log(typeof(Infinity));
-console.log(typeof(-Infinity));
-console.log(typeof(12*'hmn'));
-console.log(typeof(NaN));
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof (12 * "hmn"));
+console.log(typeof NaN);
 
+// 3- Interpolacao(Template Strings)
+console.log(`A soma, 2 + 2 = ${2+2}`);
+console.log('A soma, 2 + 2 = ${2+2}');
+console.log(`Qualquer coisa aqui: ${console.log('TESTE')}`);
