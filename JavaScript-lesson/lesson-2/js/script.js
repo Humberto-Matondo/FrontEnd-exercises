f5043f8f4496e1b91cbc05cf79c80e5556681ba8
Humberto-Matondo <humbertomat99@gmail.com>
2024-07-07 05:09:55 +0100

    numbers e especial numbers

---
+++ JavaScript-lesson/lesson-2/js/script.js
@@ -0,0 +1,12 @@
+// 1- Number
+console.log(typeof(2));
+console.log(typeof(2.3));
+console.log(typeof(-2));
+
+// 2- ESPECIAL NUMBERS
+
+console.log(typeof(Infinity));
+console.log(typeof(-Infinity));
+console.log(typeof(12*'hmn'));
+console.log(typeof(NaN));
+
