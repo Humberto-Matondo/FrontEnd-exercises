24f9127e8305540c136b0071435eff1b6cf82fc0
Humberto-Matondo <humbertomat99@gmail.com>
2024-07-18 04:33:10 +0100

    Loops in Arrays

--- JavaScript-lesson/lessons-8/js/scripts.js
+++ JavaScript-lesson/lessons-8/js/scripts.js
@@ -1,5 +1,7 @@
 const lista = [1,2,3,4,5];
 
+console.log(lista);
+
 const a = {
     nome: "MATONDO",
 };
@@ -9,4 +11,12 @@ const b = a;
 
 console.log(a);
 console.log(b);
-console.log(lista);
+console.log(a === b);
+
+const users = ['Humberto','Carlos','Pinto','Miguel','Ricardo'];
+
+console.log('Listando usuarios:');
+
+for(let i = 0; i < users.length; i++){
+    console.log(`Usuario: ${users[i]}`);
+};
