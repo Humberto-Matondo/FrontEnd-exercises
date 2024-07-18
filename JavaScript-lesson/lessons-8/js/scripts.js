2d1b6c02f3faa4d3ac21af0e3ff583b4015cc563
Humberto-Matondo <humbertomat99@gmail.com>
2024-07-18 16:30:48 +0100

    ARRAYS: IndexOf and lastIndex

--- JavaScript-lesson/lessons-8/js/scripts.js
+++ JavaScript-lesson/lessons-8/js/scripts.js
@@ -1,6 +1,10 @@
+// ARRAYS: 
+
 const lista = [1,2,3,4,5];
 
 console.log(lista);
+
+// OBJECT: 
 
 const a = {
     nome: "MATONDO",
@@ -13,6 +17,8 @@ console.log(a);
 console.log(b);
 console.log(a === b);
 
+// ESTRUTURA DE REPETICAO EM ARRAYS: 
+
 const users = ['Humberto','Carlos','Pinto','Miguel','Ricardo'];
 
 console.log('Listando usuarios:');
@@ -21,11 +27,15 @@ for(let i = 0; i < users.length; i++){
     console.log(`Usuario: ${users[i]}`);
 };
 
+// ARRAYS: Pop and Push 
+
 users.push('Kiluanje');
 console.log(users);
 
 users.pop();
 console.log(users);
+
+// ARRAYS: shift and unshift 
 
 const letters = ['d','e','f','g','h'];
 const letter = letters.shift();
@@ -35,4 +45,15 @@ console.log(letter);
 
 letters.unshift('a','b','c');
 console.log(letters);
+
+// ARRAYS: IndexOf and lastIndex
+
+const frutas = ['Melancia','Pera','Ananas','Pera'];
+
+console.log(`Index : ${frutas.indexOf('Ananas')}`);
+console.log(`Elemento do index: ${frutas[frutas.indexOf('Ananas')]}`);
+
+console.log(`Indice/Possicao do ultimo repetido: ${frutas.lastIndexOf('Pera')}`);
+
+// ARRAYS: slice
 
