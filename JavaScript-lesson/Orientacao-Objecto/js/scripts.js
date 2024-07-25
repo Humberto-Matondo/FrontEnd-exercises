dfd3bc9d11d243b10dd8480563d8d004014750da
Humberto-Matondo <humbertomat99@gmail.com>
2024-07-25 10:01:54 +0100

    Aprofundando em Metodos

--- JavaScript-lesson/Orientacao-Objecto/js/scripts.js
+++ JavaScript-lesson/Orientacao-Objecto/js/scripts.js
@@ -10,3 +10,27 @@ const animal = {
 console.log(animal.nome);
 animal.latir();
 
+// Aprofundando em Metodos
+console.log('\n\n');
+
+const pessoa = {
+    nome: 'Matondo',
+
+    getNome: function(){
+        return this.nome; // vai retornar o nome Matondo.
+    },
+
+    setNome: function(novoNome){
+        this.nome = novoNome; // Vai mudar o nome Matondo para um novo nome.
+    },
+
+
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.getNome());
+
+pessoa.setNome('Humberto');
+
+console.log(pessoa.nome);
+console.log(pessoa.getNome());
