acadacfb028af9fefaacb33ecd9e67267a0c4e7a
Humberto-Matondo <humbertomat99@gmail.com>
2024-07-29 10:31:33 +0100

    Funcoes setInterval

--- JavaScript-lesson/Programacao-Assincrona/js/scripts.js
+++ JavaScript-lesson/Programacao-Assincrona/js/scripts.js
@@ -4,6 +4,17 @@ console.log('AINDA N EXECUTOU');
 
 setTimeout(function(){ //Funcao anonima
     console.log('REQUISICOES ASSICRONAS');
-}, 2000); // 2000 e a quantidade de tempo depois, deve ser executada essa funcao
+}, 2000); // 2000ms e a quantidade de tempo depois, deve ser executada essa funcao
 
 console.log('AINDA N EXECUTOU 2');
+
+// Funcoes setInterval
+console.log('\n\n')
+
+console.log('AINDA N EXECUTOU setInterval');
+
+setInterval(function(){
+    console.log('Intervado Assincrono.');
+}, 3000); //vai ficar executando de 3000ms em 3000ms, um loop infinito. 
+
+console.log('AINDA N EXECUTOU setInterval');
