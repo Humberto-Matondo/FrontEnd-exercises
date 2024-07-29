9747bbb8ab89effa043dbdd483fb7e480bfd0e44
Humberto-Matondo <humbertomat99@gmail.com>
2024-07-29 10:50:10 +0100

    Promises

--- JavaScript-lesson/Programacao-Assincrona/js/scripts.js
+++ JavaScript-lesson/Programacao-Assincrona/js/scripts.js
@@ -13,8 +13,24 @@ console.log('\n\n')
 
 console.log('AINDA N EXECUTOU setInterval');
 
-setInterval(function(){
-    console.log('Intervado Assincrono.');
-}, 3000); //vai ficar executando de 3000ms em 3000ms, um loop infinito. 
+//setInterval(function(){
+//    console.log('Intervado Assincrono.');
+//}, 3000); //vai ficar executando de 3000ms em 3000ms, um loop infinito. 
 
 console.log('AINDA N EXECUTOU setInterval');
+
+// Promises
+console.log('\n\n')
+
+const promises = Promise.resolve(20+6); //Prometeu resolver esse problema 
+
+console.log('Um codigo');
+
+promises.then(value => {    
+    console.log(`A soma e ${value}`); //Mostrou o resultado do problema prometido, no final de tudo.
+    return value; //Se quiseres voltar a usar o resultado na promise, precisas retornar o valor. 
+}) //Essa parte a baixo e tudo opcional, caso queiraas voltar a usar o resultado
+.then((value) => value - 6)
+.then((value) => console.log(`Agora essa e a soma: ${value}`));
+
+console.log('Outro codigo');
